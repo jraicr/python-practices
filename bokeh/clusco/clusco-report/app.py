1f27ba72da8696502e9682a3ceb35b2ecd3557e8
Rai <jraicr@gmail.com>
2023-03-30 13:11:39 +0100

    Some advances in CLUSCO plots reporting system

--- bokeh/clusco/clusco-report/app.py
+++ bokeh/clusco/clusco-report/app.py
@@ -1,57 +1,47 @@
-# 6 differents plots
-# X axis: Date and hour of data
-# Y axis - plot 1: Pacta temperature (deg)
-# Y axis - plot 2: SCB temp (deg)
-# Todos comparten mismo eje X: Fecha y hora del dato
-
+import pandas as pd
 import pymongo
-import pandas
+from bson.objectid import ObjectId
 from bokeh.models import ColumnDataSource
-from bokeh.models.tools import HoverTool
-from bokeh.plotting import curdoc, figure
-
-# Html doc reference
-doc = curdoc()
+from bokeh.plotting import figure
+from bokeh.server.server import Server
 
 # Configure database connection
-mongoURI = 'mongodb://localhost:27017/'
-mongoClient = pymongo.MongoClient(mongoURI)
+mongodbURI = 'mongodb://localhost:27017/'
+mongoClient = pymongo.MongoClient(mongodbURI)
 db = mongoClient["CACO"]
+clusco_hour_data = db['CLUSCO_hour']
 
-# Get data from CLUSCO_hour collection
-clusco_hour_collection = db['CLUSCO_hour']
+# Query to get data from SCB_pixel_temperature_query parameter name in collection
+SCB_pixel_temperature_query = { 'name' : 'scb_pixel_temperature' }
 
-# SCB Temperature values
-SCB_temperature_query = { 'name' : 'scb_temperature' }
-clusco_SCB_temperature_data = pandas.DataFrame(list(clusco_hour_collection.find(SCB_temperature_query)))
+# Define the function that will create the plot
+def make_plot(doc):
+    # Create a plot with a datetime x-axis type
+    plot = figure(x_axis_type="datetime", title="SCB pixel temperature", height=350, width=800)
+    
+    # Query the database for the data
+    cursor_SCB_pixel_temperature_docs = clusco_hour_data.find(SCB_pixel_temperature_query) 
+    df_SCB_pixel_temperature = pd.DataFrame(list(cursor_SCB_pixel_temperature_docs))
+    
+    # Convert date column to datetime from df_SCB_pixel_temperature
+    df_SCB_pixel_temperature['date'] = pd.to_datetime(df_SCB_pixel_temperature['date'])  
+    
+    # Convert the ObjectId to a string
+    df_SCB_pixel_temperature['_id'] = df_SCB_pixel_temperature['_id'].apply(str)
+    
+    # Create a ColumnDataSource from the DataFrame
+    source = ColumnDataSource(df_SCB_pixel_temperature)
+    
+    # Plot the data using multi_line
+    plot.multi_line('date', 'avg', color='red', alpha=0.5, source=source)
+    
+    # Add the plot to the document
+    doc.add_root(plot)
 
-source = ColumnDataSource({'x':[clusco_SCB_temperature_data['avg']], 'y':[clusco_SCB_temperature_data['date']]})
+# Start the Bokeh server
+server = Server({'/': make_plot})
+server.start()
 
-plot = figure()
-plot.line('x', 'y', source = source)
-plot.xaxis.axis_label = 'x'
-plot.yaxis.axis_label = 'y'
-
-doc.add_root(plot)
-
-
-
-# db = mongoClient["CACO"]
-# clusco_hour_data = db['CLUSCO_hour']
-
-# SCB_Temp_query = { 'name' : 'scb_temperature' }
-# cursor_SCB_Temp_docs = clusco_hour_data.find(SCB_Temp_query)
-
-# source = ColumnDataSource(cursor_SCB_Temp_docs)
-# y_values = ['avg', 'max', 'min']
-
-# print(y_values)
-
-#doc = curdoc()
-
-#plot_size = {'width': 500, 'height': 500}
-
-# PLOT 1
-
-
-
+# Open the application in a new browser tab
+server.io_loop.add_callback(server.show, "/")
+server.io_loop.start()
--- bokeh/clusco/clusco-report/readme.md
+++ bokeh/clusco/clusco-report/readme.md
@@ -1,14 +1,33 @@
 Data to have into account based on PDF
 
 Todos los valores de los módulos:
-- PACTA Temperature
-- SCB Temperature
-- SCB Humidity 
-- Anode Current (uA)
-- Charge (C)
-- HV (V) 
 
 
-Same as before but only three values for each: average, max and min.
+- PACTA temperature
+    * property name: scb_pixel_temperature
+    * size: time x 1855 pixels
+
+- SCB temperature
+    * property name: scb_temperature
+    * size: time x 265 modules
+
+- SCB humidity
+    * property name: scb_humidity
+    * size: time x 265 modules
+
+- Anode current
+    * property name: scb_pixel_an_current
+    * size: time x 1855 pixels
+
+- HV
+    * property name: scb_pixel_hv_monitored
+    * size: time x 1855 pixels
+
+Then, please plot the following values:
+    - average over the camera
+    - minimum
+    - maximum
+    (- values of all pixels/modules when people want to have a look)
+    (- values of selected pixels/modules if possible)
 
 
